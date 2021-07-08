4d138f7f0a20b97fe07260b317031d6d578b3698
Ryodai Makino <davinci0327@icloud.com>
2021-07-08 22:19:59 +0900

    graphqlのエンドポイントを準備

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene_django'
+    'graphene_django',
     'account',
 ]
 
