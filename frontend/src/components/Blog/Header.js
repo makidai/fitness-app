13088fff949624e35ad2ae9b209b722733cd250d
Ryodai Makino <davinci0327@icloud.com>
2021-07-17 18:58:11 +0900

    文言日修正

--- frontend/src/components/Blog/Header.js
+++ frontend/src/components/Blog/Header.js
@@ -3,8 +3,6 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 
@@ -32,7 +30,6 @@ export default function Header(props) {
     return (
         <React.Fragment>
             <Toolbar className={classes.toolbar}>
-                <Button size="small">Subscribe</Button>
                 <Typography
                     component="h2"
                     variant="h5"
@@ -43,11 +40,8 @@ export default function Header(props) {
                 >
                     {title}
                 </Typography>
-                <IconButton>
-                    <SearchIcon />
-                </IconButton>
                 <Button variant="outlined" size="small">
-                    Sign up
+                    新規登録
                 </Button>
             </Toolbar>
             <Toolbar
