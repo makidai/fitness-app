7acf9e90cb42d15f8e12974259410cd65791a736
Ryodai Makino <davinci0327@icloud.com>
2021-07-18 15:20:43 +0900

    react-apolloの追加

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,29 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import {
     ApolloClient,
-    InMemoryCache,
     ApolloProvider,
-    createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+    createBatchingNetworkInterface,
+} from "react-apollo";
 
-const httpLink = createHttpLink({
-    uri: "http://127.0.0.1:8000/graphql/",
-});
-
-const authLink = setContext((_, { headers }) => {
-    const token = window.localStorage.getItem("token");
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `JWT ${token}` : "",
-        },
-    };
+const networkInterface = createBatchingNetworkInterface({
+    uri: "http://localhost:8000/gql",
+    batchInterval: 10,
+    opts: {
+        credentials: "same-origin",
+    },
 });
 
 const client = new ApolloClient({
-    uri: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    networkInterface: networkInterface,
 });
 
 ReactDOM.render(
